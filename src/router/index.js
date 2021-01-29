2e2a7235bec786013afbb8a0a2c38db182e9c8dc
liuqiang <liuqiang1357@163.com>
2021-01-29 15:21:35 +0800

    add vue router base

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import { IN_PRODUCTION, TARGET_MAINNET } from '@/utils/env';
 
 Vue.use(VueRouter);
 
@@ -17,6 +18,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
+  base: !IN_PRODUCTION || TARGET_MAINNET ? '/' : '/testnet/',
   mode: 'history',
   routes: routes.filter(route => route),
 });
